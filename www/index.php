7c3c3b0c596eedad21156a60713765b1bed9fcf3
rsparapa (Rodney Sparapani) <rsparapa@mcw.edu>
2013-11-25 20:43:07 +0000

    test 1
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/bivpack@5 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/index.php
+++ www/index.php
@@ -43,7 +43,11 @@ echo $contents; } ?>
 
 <!-- end of project description -->
 
-<p> No content added. </p>
+<p> 
+
+<a href="http://r-forge.r-project.org/themes/rforge/imagesrf/banners/Rlogo.jpg">R logo</a>
+
+No content added. </p>
 
 <p> The <strong>project summary page</strong> you can find <a href="http://<?php echo $domain; ?>/projects/<?php echo $group_name; ?>/"><strong>here</strong></a>. </p>
 
